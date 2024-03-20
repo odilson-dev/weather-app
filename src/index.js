5bcb9c88c8741715e5dd932d25fe0edd888c86fb
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-20 12:02:29 -0400

    Add image loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
+import Background from "./images/background.jpg";
 
+const myBackground = new Image();
+myBackground.src = Background;
 // async function getCats() {
 //   const response = await fetch(
 //     "https://api.weatherapi.com/v1/current.json?key=de60dbcadbfb42369c1201306241703&q=port-au-prince",
