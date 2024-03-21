56c7c98e77dd2a13a81bcf5f1a8f0d792c49a42c
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-21 00:13:24 -0400

    Update details for each region

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
 import "./style.css";
 const img = require("./images/cloud.png");
-import Background from "./images/background.jpg";
 
-const myBackground = new Image();
-myBackground.src = Background;
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+
+search.addEventListener("click", () => {
+  const APIKey = "b40dacdf727ab9a1523506ac015d99d8";
+  const city = document.querySelector(".search-box input").value;
+
+  if (city == "") {
+    return;
+  }
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      const image = document.querySelector(".weather-box img");
+      const temperature = document.querySelector(".weather-box .temperature");
+      const description = document.querySelector(".weather-box .description");
+      const humidity = document.querySelector(
+        ".weather-details .humidity span"
+      );
+      const wind = document.querySelector(".weather-details .wind span");
+
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+        case "Rain":
+          image.src = "images/rain.png";
+          break;
+        case "Snow":
+          image.src = "images/snow.png";
+          break;
+
+        case "Clouds":
+          image.src = "images/cloud.png";
+          break;
+        case "Mist":
+          image.src = "images/mist.png";
+          break;
+
+        case "Hast":
+          image.src = "images/mist.png";
+          break;
+
+        default:
+          image.src = "images/cloud.png";
+          break;
+      }
+
+      temperature.innerHTML = `${parseInt(json.main.temp)} <span>℃</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+    });
+});
+
 // async function getCats() {
 //   const response = await fetch(
 //     "https://api.weatherapi.com/v1/current.json?key=de60dbcadbfb42369c1201306241703&q=port-au-prince",
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
